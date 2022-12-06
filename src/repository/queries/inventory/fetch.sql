a59e31f38134dbb750a45926fa4f32e93d13d546
sean <sean.stewart@mavenclinic.com>
2022-12-06 12:38:24 -0500

    Solve day 3, part 2

--- src/repository/queries/inventory/fetch.sql
+++ src/repository/queries/inventory/fetch.sql
@@ -1,2 +1,4 @@
 -- :name calculate_overlap_magnitude :one
-SELECT * FROM aoc.calculate_overlap_magnitude(inventory_id => :inventory_id::bigint);
+SELECT * FROM aoc.calculate_overlap_magnitude_by_group(
+    inventory_id => :inventory_id::bigint
+);
